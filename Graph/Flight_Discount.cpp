4b64817def5cc1b378f2c8024c199509852a9e78
harsh6975 <harshu1234ashv@gmail.com>
2022-09-29 01:15:54 +0530

    Flight_Discount

---
+++ Graph/Flight_Discount.cpp
@@ -0,0 +1,263 @@
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <climits>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <stack>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
+typedef pair<double, double> pd;
+typedef vector<vector<pl>> vvpl;
+typedef vector<pl> vpl;
+typedef vector<pi> vpi;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<string>> vvs;
+typedef vector<vector<char>> vvc;
+ll MOD = 1000000007;
+#define FOR0(i, n) for (ll i = 0; i < n; i++)
+#define FORa(i, a, b) for (ll i = a; i < b; i++)
+#define rFOR0(i, s) for (ll i = s; i >= 0; i--)
+#define rFORa(i, s, e) for (ll i = s; i >= e; i--)
+#define PB push_back
+#define F first
+#define S second
+#define fast_cin()                  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+#define T      \
+  int test;    \
+  cin >> test; \
+  while (test--)
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+ll gcd(ll a, ll b)
+{
+  if (a == 0)
+    return b;
+  return gcd(b % a, a);
+}
+
+ll merge(ll x, ll y)
+{
+  return x + y;
+}
+void buildSegTree(vl &v, vl &segTree, ll root, ll l, ll r)
+{
+  if (l == r)
+  {
+    segTree[root] = v[l];
+    return;
+  }
+  ll mid = (l + r) / 2;
+  buildSegTree(v, segTree, 2 * root, l, mid);
+  buildSegTree(v, segTree, 2 * root + 1, mid + 1, r);
+  segTree[root] = merge(segTree[2 * root], segTree[2 * root + 1]);
+}
+void updateSegTree(vl &segTree, ll root, ll l, ll r, ll index, ll val)
+{
+  if (l == r)
+  {
+    segTree[root] = val;
+    return;
+  }
+  ll mid = (l + r) / 2;
+  if (index <= mid)
+  {
+    updateSegTree(segTree, 2 * root, l, mid, index, val);
+  }
+  else
+  {
+    updateSegTree(segTree, 2 * root + 1, mid + 1, r, index, val);
+  }
+  segTree[root] = merge(segTree[2 * root], segTree[2 * root + 1]);
+}
+int findQuery(vl &segTree, ll root, ll l, ll r, ll lq, ll rq)
+{
+  if (lq <= l && r <= rq)
+  {
+    return segTree[root];
+  }
+  ll mid = (l + r) / 2;
+  if (mid < lq)
+  {
+    return findQuery(segTree, 2 * root + 1, mid + 1, r, lq, rq);
+  }
+  if (mid + 1 > rq)
+  {
+    return findQuery(segTree, 2 * root, l, mid, lq, rq);
+  }
+  auto left = findQuery(segTree, 2 * root, l, mid, lq, rq);
+  auto right = findQuery(segTree, 2 * root + 1, mid + 1, r, lq, rq);
+  return merge(left, right);
+}
+void RangeUpdateSegTree(vl &segTree, ll root, ll l, ll r, ll lq, ll rq, ll val)
+{
+  if (r < lq || l > rq)
+    return;
+  if (lq <= l && r <= rq)
+  {
+    segTree[root] += val;
+    return;
+  }
+  ll mid = (l + r) / 2;
+  RangeUpdateSegTree(segTree, 2 * root, l, mid, lq, rq, val);
+  RangeUpdateSegTree(segTree, 2 * root + 1, mid + 1, r, lq, rq, val);
+}
+ll pointFindQuery(vl &segTree, ll root, ll l, ll r, ll index)
+{
+  if (l == r)
+  {
+    return segTree[root];
+  }
+  int mid = (l + r) / 2;
+  if (index <= mid)
+  {
+    return segTree[root] + pointFindQuery(segTree, 2 * root, l, mid, index);
+  }
+  else
+  {
+    return segTree[root] + pointFindQuery(segTree, 2 * root + 1, mid + 1, r, index);
+  }
+}
+
+class Node
+{
+public:
+  ll a, b, c;
+  Node(ll x, ll y, ll z)
+  {
+    a = x;
+    b = y;
+    c = z;
+  }
+};
+
+class mysort
+{
+public:
+  bool operator()(Node &a, Node &b)
+  {
+    if (a.a == b.a)
+    {
+      return a.b < b.b;
+    }
+    return a.a > b.a;
+  }
+};
+
+int sol1(vvpl &graph)
+{
+  int n = graph.size();
+  vvl dis(n, vl(2, 1e16));
+  priority_queue<Node, vector<Node>, mysort> q;
+  q.push(Node(0, 1, 0));
+  dis[0][1] = 0;
+  ll ans = 0;
+
+  while (!q.empty())
+  {
+    auto curr = q.top();
+    q.pop();
+    for (auto x : graph[curr.c])
+    {
+      if (dis[x.first][curr.b] > curr.a + x.second)
+      {
+        dis[x.first][curr.b] = curr.a + x.second;
+        q.push(Node(dis[x.first][curr.b], curr.b, x.first));
+      }
+      if (curr.b - 1 >= 0 && dis[x.first][curr.b - 1] > curr.a + x.second / 2)
+      {
+        dis[x.first][curr.b - 1] = curr.a + x.second / 2;
+        q.push(Node(dis[x.first][curr.b - 1], curr.b - 1, x.first));
+      }
+    }
+  }
+
+  return min(dis[n - 1][0], dis[n - 1][1]);
+}
+
+void dijkstra(vvpl &graph, vl &dis, int sc)
+{
+  int n = graph.size();
+
+  vl vis(n);
+  priority_queue<pl, vpl, greater<pl>> q;
+  q.push({0, sc});
+  dis[sc] = 0;
+  while (!q.empty())
+  {
+    auto curr = q.top();
+    q.pop();
+    if (vis[curr.second])
+      continue;
+    vis[curr.second] = 1;
+    for (auto x : graph[curr.second])
+    {
+      if (dis[x.first] > curr.first + x.second)
+      {
+        dis[x.first] = curr.first + x.second;
+        q.push({dis[x.first], x.first});
+      }
+    }
+  }
+}
+
+int main()
+{
+  // read the question correctly (ll vs int)
+  fast_cin();
+
+  int n, m;
+  cin >> n >> m;
+  vvl edge;
+  vvpl graph1(n), graph2(n);
+  FOR0(i, m)
+  {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    a--;
+    b--;
+    edge.push_back({a, b, c});
+    graph1[a].push_back({b, c});
+    graph2[b].push_back({a, c});
+  }
+  // cout << sol1(graph);
+
+  vl dis1(n, 1e16), dis2(n, 1e16);
+
+  dijkstra(graph1, dis1, 0);
+  dijkstra(graph2, dis2, n - 1);
+
+  // FOR0(i,n)cout<<dis1[i]<<" ";
+  // cout<<endl;
+  // FOR0(i,n)cout<<dis2[i]<<" ";
+  // cout<<endl;
+  ll ans = 1e16;
+  for (auto v : edge)
+  {
+    ans = min(ans, dis1[v[0]] + dis2[v[1]] + v[2] / 2);
+  }
+  cout << ans;
+  return 0;
+}
