5d693a576ba69213cfbe72b8eef20820a3af8f82
harsh6975 <harshu1234ashv@gmail.com>
2022-10-27 02:02:59 +0530

    Tree_Disatances_II

---
+++ Tree algorithm/Tree_Distances_II.cpp
@@ -0,0 +1,206 @@
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <climits>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <stack>
+#include <iomanip>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
+typedef pair<double, double> pd;
+typedef vector<vector<pl>> vvpl;
+typedef vector<pl> vpl;
+typedef vector<pi> vpi;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<string>> vvs;
+typedef vector<vector<char>> vvc;
+ll MOD = 1000000007;
+#define FOR0(i, n) for (ll i = 0; i < n; i++)
+#define FORa(i, a, b) for (ll i = a; i < b; i++)
+#define rFOR0(i, s) for (ll i = s; i >= 0; i--)
+#define rFORa(i, s, e) for (ll i = s; i >= e; i--)
+#define PB push_back
+#define F first
+#define S second
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define T        \
+    int test;    \
+    cin >> test; \
+    while (test--)
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+ll merge(ll x, ll y)
+{
+    return x + y;
+}
+void buildSegTree(vl &v, vl &segTree, ll root, ll l, ll r)
+{
+    if (l == r)
+    {
+        segTree[root] = v[l];
+        return;
+    }
+    ll mid = (l + r) / 2;
+    buildSegTree(v, segTree, 2 * root, l, mid);
+    buildSegTree(v, segTree, 2 * root + 1, mid + 1, r);
+    segTree[root] = merge(segTree[2 * root], segTree[2 * root + 1]);
+}
+void updateSegTree(vl &segTree, ll root, ll l, ll r, ll index, ll val)
+{
+    if (l == r)
+    {
+        segTree[root] = val;
+        return;
+    }
+    ll mid = (l + r) / 2;
+    if (index <= mid)
+    {
+        updateSegTree(segTree, 2 * root, l, mid, index, val);
+    }
+    else
+    {
+        updateSegTree(segTree, 2 * root + 1, mid + 1, r, index, val);
+    }
+    segTree[root] = merge(segTree[2 * root], segTree[2 * root + 1]);
+}
+int findQuery(vl &segTree, ll root, ll l, ll r, ll lq, ll rq)
+{
+    if (lq <= l && r <= rq)
+    {
+        return segTree[root];
+    }
+    ll mid = (l + r) / 2;
+    if (mid < lq)
+    {
+        return findQuery(segTree, 2 * root + 1, mid + 1, r, lq, rq);
+    }
+    if (mid + 1 > rq)
+    {
+        return findQuery(segTree, 2 * root, l, mid, lq, rq);
+    }
+    auto left = findQuery(segTree, 2 * root, l, mid, lq, rq);
+    auto right = findQuery(segTree, 2 * root + 1, mid + 1, r, lq, rq);
+    return merge(left, right);
+}
+void RangeUpdateSegTree(vl &segTree, ll root, ll l, ll r, ll lq, ll rq, ll val)
+{
+    if (r < lq || l > rq)
+        return;
+    if (lq <= l && r <= rq)
+    {
+        segTree[root] += val;
+        return;
+    }
+    ll mid = (l + r) / 2;
+    RangeUpdateSegTree(segTree, 2 * root, l, mid, lq, rq, val);
+    RangeUpdateSegTree(segTree, 2 * root + 1, mid + 1, r, lq, rq, val);
+}
+ll pointFindQuery(vl &segTree, ll root, ll l, ll r, ll index)
+{
+    if (l == r)
+    {
+        return segTree[root];
+    }
+    int mid = (l + r) / 2;
+    if (index <= mid)
+    {
+        return segTree[root] + pointFindQuery(segTree, 2 * root, l, mid, index);
+    }
+    else
+    {
+        return segTree[root] + pointFindQuery(segTree, 2 * root + 1, mid + 1, r, index);
+    }
+}
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+bool safe(int x, int y, int n, int m)
+{
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+void cal_indp(vvl &graph, vl &inDp, vl &szi, int sc, int par)
+{
+    szi[sc] = 1;
+    inDp[sc] = 0;
+    for (auto x : graph[sc])
+    {
+        if (x == par)
+            continue;
+        cal_indp(graph, inDp, szi, x, sc);
+        szi[sc] += szi[x];
+        inDp[sc] += inDp[x] + szi[x];
+    }
+}
+
+void cal_outdp(vvl &graph, vl &inDp, vl &outDp, vl &szi, int sc, int par)
+{
+    int n = sz(graph);
+    if (par != -1)
+    {
+        outDp[sc] = (outDp[par] + n - szi[par]) + (inDp[par] - inDp[sc] - szi[sc]) + szi[par] - szi[sc];
+    }
+
+    for (auto x : graph[sc])
+    {
+        if (x == par)
+            continue;
+        ;
+        cal_outdp(graph, inDp, outDp, szi, x, sc);
+    }
+}
+
+int main()
+{
+    // read the question correctly (ll vs int)
+    fast_cin();
+    int n;
+    cin >> n;
+    vvl graph(n);
+    FOR0(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        graph[a - 1].push_back(b - 1);
+        graph[b - 1].push_back(a - 1);
+    }
+
+    vector<ll> inDp(n), outDp(n), szi(n);
+    cal_indp(graph, inDp, szi, 0, -1);
+    cal_outdp(graph, inDp, outDp, szi, 0, -1);
+    FOR0(i, n)
+    {
+        cout << inDp[i] + outDp[i] << " ";
+    }
+
+    return 0;
+}
